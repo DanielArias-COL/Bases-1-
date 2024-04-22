f45737470d74dc906f851ab0be7f824f5ec8b058
escanor904 <daariasa_1@uqvirtual.edu.co>
2024-04-22 02:07:54 -0500

    feat(empleado): interfaz,tabla y add

--- date/empleadoDate.py
+++ date/empleadoDate.py
@@ -1,10 +1,10 @@
 
-from connection import MySQLConnection
+from date.connection import MySQLConnection
 from model.empleado import Empleado
 
 class EmpleadoDate():
     
-    def consultarEmpleados():
+    def consultarEmpleados_tabla(self):
         empleados = []
         connection = MySQLConnection()
         connection.connect()
@@ -13,18 +13,69 @@ class EmpleadoDate():
         query = "SELECT * FROM empleado"
         result = connection.execute_query(query)
 
-        # Recorrer los resultados y crear instancias de Empleado
+        # Recorrer los resultados y crear diccionarios
         for empleado_data in result:
-            empleado = Empleado(*empleado_data)
-            empleados.append(empleado)
+            empleado_dict = {
+                "Codigo": empleado_data[0],
+                "Cedula": empleado_data[1],
+                "Nombre": empleado_data[2],
+                "Direccion": empleado_data[3],
+                "Telefono": empleado_data[4]
+                # Agrega más claves y valores según la estructura de la tabla 'empleado'
+            }
+            empleados.append(empleado_dict)
 
         connection.disconnect()
 
         return empleados
 
-# Ejemplo de uso:
-if __name__ == "__main__":
-    lista_empleados = EmpleadoDate.consultarEmpleados()
+    def buscar_empleado(self, empleado_id):
+        # Crear una instancia de MySQLConnection y establecer la conexión
+        connection = MySQLConnection()
+        connection.connect()
 
-    for empleado in lista_empleados:
-        print(empleado._nombre)  # Acceder al nombre del empleado
+        # Construir la consulta SQL utilizando el ID del empleado proporcionado
+        query = f"SELECT * FROM empleado WHERE emp_id = {empleado_id}"
+
+        try:
+
+            result = connection.execute_query(query)
+            return result
+
+        except Exception as E:
+
+            print("entra a el error")
+            return E
+        
+                
+        finally:
+            # Cerrar la conexión con la base de datos en cualquier caso
+            connection.disconnect()
+    
+    def agregar_empleado(self, id, cedula, nombre, direccion, telefono):
+
+        connection = MySQLConnection()
+        connection.connect()
+
+        # Construir la consulta SQL utilizando el ID del empleado proporcionado
+        query = f"""
+            INSERT INTO empleado (emp_id, cedula, nombre, direccion, telefono) 
+	            VALUES ({id}, '{cedula}', '{nombre}', '{direccion}', '{telefono}');
+        """
+        try:
+            # Ejecutar la consulta SQL utilizando la conexión establecida
+            connection.execute_query(query)
+            connection.connection.commit()
+
+        except Exception as E:
+            print("entro a el error")
+            return E
+        
+        finally:
+            # Cerrar la conexión con la base de datos en cualquier caso
+            connection.disconnect()
+
+
+    
+
+
--- ui/empleado.py
+++ ui/empleado.py
@@ -1,11 +1,153 @@
 from PyQt6 import uic 
 
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QTableWidgetItem, QPushButton, QMessageBox
+
+from date.empleadoDate import EmpleadoDate
 
 
 class VentanaEmpleado(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi('ui\empleado_form.ui',self)
+        
+
+        # Connect UI elements to class variables
+        self.empleado_id = self.ui.lineEditCodigo
+        self.empleado_cedula = self.ui.lineEditCedula
+        self.empleado_nombre = self.ui.lineEditNombre
+        self.empleado_direccion = self.ui.lineEditDireccion
+        self.empleado_telefono = self.ui.lineEditTelefono
+
+        self.result_table = self.ui.tableWidget
+        self.result_table.setSortingEnabled(False)
+        self.buttons_list = self.ui.function_frame.findChildren(QPushButton)
+        
+        # Initialize signal-slot connections
+        self.init_signal_slot()
+        
+        self.empleado_date = EmpleadoDate()
+        result = self.empleado_date.consultarEmpleados_tabla()
+        self.show_data(result)
         self.ui.show()
+
+
+    def init_signal_slot(self):
+        # Connect buttons to their respective functions
+        self.add_btn.clicked.connect(self.add_info)
+        self.search_btn.clicked.connect(self.search_info)
+        self.clear_btn.clicked.connect(self.clear_form_info)
+        #self.select_btn.clicked.connect(self.select_info)
+        #self.update_btn.clicked.connect(self.update_info)
+        #self.delete_btn.clicked.connect(self.delete_info)
+
+    def disable_buttons(self):
+        # Disable all buttons
+        for button in self.buttons_list:
+            button.setDisabled(True)
+
+    def enable_buttons(self):
+        # Enable all buttons
+        for button in self.buttons_list:
+            button.setDisabled(False)    
+
+    def add_info(self):
+        # Function to add student information
+        self.disable_buttons()
+
+        student_info = self.get_empleado_info()
+
+        if student_info["Codigo"] and student_info["Cedula"] and student_info["Nombre"] and student_info["Telefono"] :
+            check_result = self.check_empleado_id(int(student_info["Codigo"]))
+
+            
+            if check_result:
+                QMessageBox.information(self, "Warning", "Por favor ingrese un nuevo código para el empleado",
+                                        QMessageBox.StandardButton.Ok)
+                self.enable_buttons()
+                return
+            
+            print(student_info["Telefono"]+"telefonoooo")
+
+            add_result = self.empleado_date.agregar_empleado(int(student_info["Codigo"]),
+                                                       int(student_info["Cedula"]),
+                                                       student_info["Nombre"],
+                                                       student_info["Direccion"],
+                                                       int(student_info["Telefono"]))
+                                          
+
+            if add_result:
+                QMessageBox.information(self, "Warning", f"Add fail: {add_result}, Please try again.",
+                                        QMessageBox.StandardButton.Ok)
+
+        else:
+            QMessageBox.information(self, "Warning", "Please input student ID and first name.",
+                                    QMessageBox.StandardButton.Ok)
+
+        self.search_info()
+        self.enable_buttons()
+
+    def search_info(self):
+        # Function to search for student information and populate the table
+        result = self.empleado_date.consultarEmpleados_tabla()
+        self.show_data(result)
+   
+    def clear_form_info(self):
+        # Function to clear the form
+        self.empleado_id.clear()
+        self.empleado_cedula.clear()
+        self.empleado_nombre.clear()
+        self.empleado_direccion.clear()
+        self.empleado_telefono.clear()
+        
+        
+
+    def check_empleado_id(self, id):
+        # Function to check if a student ID already exists
+        result = self.empleado_date.buscar_empleado(empleado_id=id)
+        return result
+
+    def get_empleado_info(self):
+        # Function to retrieve student information from the form
+        empleado_id = self.empleado_id.text().strip()
+        cedula = self.empleado_cedula.text().strip()
+        nombre = self.empleado_nombre.text().strip()
+        direccion = self.empleado_direccion.text().strip()
+        telefono = self.empleado_telefono.text().strip()
+        
+
+        student_info = {
+            "Codigo": empleado_id,
+            "Cedula": cedula,
+            "Nombre": nombre,
+            "Direccion": direccion,
+            "Telefono": telefono,
+        }
+
+        return student_info
+    
+    def show_data(self, result):
+        # Function to populate the table with student information
+        ## Función para llenar la tabla con información del estudiante
+        if result:
+            self.result_table.setRowCount(0)
+            self.result_table.setRowCount(len(result))
+
+            #saca el indice y la fila de la tabla
+            for row, info in enumerate(result):
+                
+                info_list = [
+                    info["Codigo"],
+                    info["Cedula"],
+                    info["Nombre"],
+                    info["Direccion"],
+                    info["Telefono"],
+                ]
+
+                for column, item in enumerate(info_list):
+                    cell_item = QTableWidgetItem(str(item))
+                    self.result_table.setItem(row, column, cell_item)
+
+        else:
+            self.result_table.setRowCount(0)
+            return
 
